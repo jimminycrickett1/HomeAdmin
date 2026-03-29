2814f7cf323c5741e37497680b207a9e81b2cb70
jimminycrickett1 <jimminycrickett@pm.me>
2026-03-29 17:32:44 -0600

    Document SQLite M3 storage strategy and lock backend contract

---
+++ src/homeadmin/discovery/__init__.py
@@ -0,0 +1,7 @@
+"""Discovery exports."""
+
+from __future__ import annotations
+
+from homeadmin.discovery.workflow import DiscoverResult, run_discovery
+
+__all__ = ["DiscoverResult", "run_discovery"]
