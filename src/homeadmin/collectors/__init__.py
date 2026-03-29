a91a674614ea842c487f936b17fc1d432b429c22
jimminycrickett1 <jimminycrickett@pm.me>
2026-03-29 15:42:22 -0600

    Add safe arp-scan and nmap collectors with artifact provenance

--- src/homeadmin/collectors/__init__.py
+++ src/homeadmin/collectors/__init__.py
@@ -1,1 +1,17 @@
 """collectors package."""
+
+from .arp_scan import ArtifactMetadata as ArpScanArtifactMetadata
+from .arp_scan import CollectorRecord as ArpScanRecord
+from .arp_scan import collect_arp_scan
+from .nmap import ArtifactMetadata as NmapArtifactMetadata
+from .nmap import CollectorRecord as NmapRecord
+from .nmap import collect_nmap
+
+__all__ = [
+    "ArpScanArtifactMetadata",
+    "ArpScanRecord",
+    "NmapArtifactMetadata",
+    "NmapRecord",
+    "collect_arp_scan",
+    "collect_nmap",
+]
